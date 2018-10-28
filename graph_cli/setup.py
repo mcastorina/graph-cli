c5cbfea4ea79395837507a0075331cab036c6cff
Miccah Castorina <m.castorina93@gmail.com>
2018-10-27 19:59:41 -0500

    Version 0.0.4

--- graph_cli/setup.py
+++ graph_cli/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="graph_cli",
-    version="0.0.3",
+    version="0.0.4",
     author="miccah",
     author_email="m.castorina93@gmail.com",
     description="A CLI utility to create graphs from CSV files.",
