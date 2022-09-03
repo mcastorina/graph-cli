25c4f6346e46a6811ee5d47e04f4db907b03701a
Miccah Castorina <m.castorina93@gmail.com>
2022-09-02 21:47:42 -0500

    Version 0.1.19

--- graph_cli/graph_cli/options.py
+++ graph_cli/graph_cli/options.py
@@ -452,6 +452,6 @@ def parse_args():
             help='use this option to combine graphs into a single image')
     parser.add_argument('--version', '-v', action='version',
             help='print version string',
-            version='graph-cli v0.1.18')
+            version='graph-cli v0.1.19')
 
     return validate_args(parser.parse_args())
